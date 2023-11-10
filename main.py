18e752abfada7fa1a42aaa58c6a1601e9a0337d1
jtbso2 <ciwkojan@gmail.com>
2023-11-10 09:59:55 +0100

    test

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("hello world")
-def nwd():
-    pass
+slw= {'Marian': 123123123 , 'Marcin': 456456456 , 'Mateusz': 789789789}
+
+slw.update({'Mikolaj': 576378567})
+y= input("Podaj imie: ")
+x= int(input("Podaj numer: "))
+
+slw.update({y: x})
+
+del slw['Marcin']
+
+for i in slw:
+    print(i , ' - ' , slw[i])
