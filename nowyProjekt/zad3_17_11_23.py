94fffbc5dfe196324c5f9b6037c1ad6346558929
jtbso2 <ciwkojan@gmail.com>
2023-11-17 10:36:49 +0100

    test

--- nowyProjekt/zad3_17_11_23.py
+++ nowyProjekt/zad3_17_11_23.py
@@ -1,1 +1,12 @@
 #napisz program ktory obliczy wartosc sinusa=1 korzystajac ze wzoru sin1= 1-1/3!+1/5!+1/7!...  obliczenia wykonaj z dokladnoscia d 4 miesc po kropce dziesietnej
+eps = 0.00001
+
+skladowa = 1
+i = 1
+wynik= 0
+while skladowa>eps:
+    i += 2
+    wynik += skladowa / i
+    skladowa /= i
+    print(skladowa)
+
