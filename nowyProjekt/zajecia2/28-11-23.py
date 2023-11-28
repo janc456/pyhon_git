5d30772e7a27cc3ecc3eb2bd829a56670f969be5
jtbso2 <ciwkojan@gmail.com>
2023-11-28 15:05:57 +0100

    wielomian

---
+++ nowyProjekt/zajecia2/28-11-23.py
@@ -0,0 +1,6 @@
+#napisz program ktory obliczy wartosc wielomianu algorytmem naiwnym wykorzystujacy funkcje pobierania argomentow o podanym stopniu wielomianow
+
+def wielomian (n):
+    lista=[]
+    for i in lista:
+        if i
