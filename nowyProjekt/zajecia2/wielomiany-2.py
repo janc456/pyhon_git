da0dda4107105bb522a9d796d6cf1554345cf098
jtbso2 <ciwkojan@gmail.com>
2023-12-01 10:30:03 +0100

    wielomian2

---
+++ nowyProjekt/zajecia2/wielomiany-2.py
@@ -0,0 +1,7 @@
+#napisz program ktory wczyta podstawe potegi oraz napis reprezetujacy wykladnik potegi w systemie binarnym a nastepnie wypisze wartosc potegi zasosuj funkcje ktora wykonalismy na lekcji
+# def potega(podstawa, wykladnik):
+#    x=bin(podstawa**wykladnik)
+#
+#    return x
+#
+# print(potega(3,2))
